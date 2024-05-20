22c8eb3b5199d6ee96dc67c33b108bc33e6863ca
AzimovAsadbek <asadbekazimov0629@gmail.com>
2024-05-21 02:26:41 +0500

    broadCrumb bag fixed

--- src/components/Generics/BroadComb/index.jsx
+++ src/components/Generics/BroadComb/index.jsx
@@ -4,15 +4,14 @@ import { useLocation } from "react-router-dom";
 
 const BroadCrumb = () => {
   const location = useLocation();
-  //
-  return (
-    location.state?.parent !== "Analitika" && (
-      <Container title={location.state?.parent}>
-        <Title>{location.state?.parent}</Title>
-        {location.state?.child && <Icon />}
-        <Title type="true">{location.state?.child}</Title>
-      </Container>
-    )
+  return location.state?.parent !== "Analitika" ? (
+    <Container title={location.state?.parent}>
+      <Title>{location.state?.parent}</Title>
+      {location.state?.child && <Icon />}
+      <Title type="true">{location.state?.child}</Title>
+    </Container>
+  ) : (
+    ""
   );
 };
 
